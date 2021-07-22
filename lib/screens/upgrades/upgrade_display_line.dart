6d681687bbbe947740b785f2b83a636f54be0797
Edward Carmack <ariemethacc@gmail.com>
2021-07-21 21:30:31 -0400

    upgrade screen now checks the mod requirements mods that fail their requirement check are disabled and the text has a strikethrough over it

--- lib/screens/upgrades/upgrade_display_line.dart
+++ lib/screens/upgrades/upgrade_display_line.dart
@@ -6,44 +6,49 @@ const int _maxUpgradeNameLines = 2;
 const int _maxUpgradeDescriptionLines = 4;
 
 class UpgradeDisplayLine extends StatelessWidget {
+  final Modification mod;
+  final Unit unit;
+
   UpgradeDisplayLine({
     Key? key,
     required this.mod,
     required this.unit,
   }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    var isModSelectable =
+        this.mod.requirementCheck(this.unit) || this.unit.hasMod(this.mod.id);
     return Row(
       children: [
         Checkbox(
-          value: unit.hasMod(mod.id),
-          onChanged: (bool? newValue) {
-            if (newValue!) {
-              unit.addUnitMod(mod);
-            } else {
-              unit.removeUnitMod(mod.id);
-            }
-          },
-        ),
+            value: unit.hasMod(mod.id),
+            onChanged: isModSelectable
+                ? (bool? newValue) {
+                    if (newValue!) {
+                      unit.addUnitMod(mod);
+                    } else {
+                      unit.removeUnitMod(mod.id);
+                    }
+                  }
+                : null),
         Text(
           '${mod.name}: ',
           style: TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.w600,
             fontStyle: FontStyle.normal,
+            decoration: isModSelectable ? null : TextDecoration.lineThrough,
           ),
           maxLines: _maxUpgradeNameLines,
         ),
-        ...createModChanges(mod),
+        ...createModChanges(mod, isSelectable: isModSelectable),
       ],
     );
   }
-
-  final Modification mod;
-  final Unit unit;
 }
 
-List<Text> createModChanges(Modification mod) {
+List<Text> createModChanges(Modification mod, {bool isSelectable = true}) {
   return mod.description.map((modName) {
     if (mod.description[mod.description.length - 1] == modName) {
       return Text(
@@ -52,6 +57,7 @@ List<Text> createModChanges(Modification mod) {
           fontSize: 16,
           fontWeight: FontWeight.normal,
           fontStyle: FontStyle.italic,
+          decoration: isSelectable ? null : TextDecoration.lineThrough,
         ),
         maxLines: _maxUpgradeDescriptionLines,
       );
@@ -62,6 +68,7 @@ List<Text> createModChanges(Modification mod) {
         fontSize: 16,
         fontWeight: FontWeight.normal,
         fontStyle: FontStyle.italic,
+        decoration: isSelectable ? null : TextDecoration.lineThrough,
       ),
     );
   }).toList();
