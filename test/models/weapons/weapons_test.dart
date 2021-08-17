8cb88c73b9c7eb11a8ec80c59adfe26a31a2827f
Edward Carmack <AriemethAcc@gmail.com>
2021-08-16 21:53:28 -0400

    changed weapons test to a table driven test design

--- test/models/weapons/weapons_test.dart
+++ test/models/weapons/weapons_test.dart
@@ -4,30 +4,51 @@ import 'package:gearforce/models/weapons/weapon_modes.dart';
 import 'package:gearforce/models/weapons/weapons.dart';
 import 'package:test/test.dart';
 
-final _sizes = ['L', 'M', 'H'];
+const _sizes = ['L', 'M', 'H'];
+
+class TestTable {
+  const TestTable({
+    required this.code,
+    required this.name,
+    required this.modes,
+    required this.range,
+    required this.damage,
+    required this.traits,
+  });
+  final String code;
+  final String name;
+  final List<weaponModes> modes;
+  final Range range;
+  final Map<String, int> damage;
+  final List<Trait> traits;
+}
 
 void main() {
-  group('test building AAMs', () {
-    const code = 'AAM';
-    final modes = [weaponModes.Direct, weaponModes.Indirect];
-    final range = Range(min: 12, short: 36, long: 72);
-    final damage = {'L': 7, 'M': 8, 'H': 9};
-    final traits = [Trait(name: 'Flak'), Trait(name: 'Guided')];
-    const name = 'Anti-Air Missile';
-
-    for (var size in _sizes) {
-      test('test building basic $size$code', () {
-        final l = buildWeapon(code: '$size$code');
-        expect(l, isNotNull, reason: 'weapon should not be null');
-        expect(l?.code, '$size$code', reason: 'check weapon code');
-        expect(l?.name, equals(name), reason: 'check name');
-        expect(l?.modes, equals(modes), reason: 'check modes');
-        expect(l?.damage, equals(damage[size]), reason: 'check damage');
-        expect(l?.range, equals(range), reason: 'check range');
-        expect(l?.traits, equals(traits), reason: 'check traits');
-      });
-    }
-  });
+  final testTables = [
+    TestTable(
+        code: 'AAM',
+        name: 'Anti-Air Missile',
+        modes: [weaponModes.Direct, weaponModes.Indirect],
+        range: Range(min: 12, short: 36, long: 72),
+        damage: {'L': 7, 'M': 8, 'H': 9},
+        traits: [Trait(name: 'Flak'), Trait(name: 'Guided')])
+  ];
+  for (var tt in testTables) {
+    group('test building ${tt.code}s', () {
+      for (var size in _sizes) {
+        test('test building basic $size${tt.code}', () {
+          final l = buildWeapon(code: '$size${tt.code}');
+          expect(l, isNotNull, reason: 'weapon should not be null');
+          expect(l?.code, '$size${tt.code}', reason: 'check weapon code');
+          expect(l?.name, equals(tt.name), reason: 'check name');
+          expect(l?.modes, equals(tt.modes), reason: 'check modes');
+          expect(l?.damage, equals(tt.damage[size]), reason: 'check damage');
+          expect(l?.range, equals(tt.range), reason: 'check range');
+          expect(l?.traits, equals(tt.traits), reason: 'check traits');
+        });
+      }
+    });
+  }
 
   test('test building combo weapon', () {
     final l = buildWeapon(code: 'LAC/LGL');
