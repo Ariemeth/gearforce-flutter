48e3ee7311dde22fce313e6ce1f37ddd8bcd7ddf
Edward Carmack <ariemethacc@gmail.com>
2021-12-19 16:37:24 -0500

    added loop to create combat group section stubs in pdf for each cg

--- lib/screens/roster/pdf/record_sheet/combat_groups.dart
+++ lib/screens/roster/pdf/record_sheet/combat_groups.dart
@@ -1,15 +1,16 @@
+import 'dart:math';
 import 'package:gearforce/models/combatGroups/combat_group.dart';
 import 'package:gearforce/models/roster/roster.dart';
 import 'package:pdf/widgets.dart' as pw;
 
-const double _standardTextSize = 12;
+//const double _standardTextSize = 12;
 const double _smallTextSize = 8;
 
-List<pw.Widget> buildCombatGroups(pw.Font font, UnitRoster roster) {
-  final standardTextStyle =
-      pw.TextStyle(font: font, fontSize: _standardTextSize);
-  final smallTextStyle = pw.TextStyle(font: font, fontSize: _smallTextSize);
+final primarySecondaryDivide =
+    pw.Padding(padding: pw.EdgeInsets.only(bottom: 5.0));
+final groupDivide = pw.Padding(padding: pw.EdgeInsets.only(bottom: 10.0));
 
+List<pw.Widget> buildCombatGroups(pw.Font font, UnitRoster roster) {
   final List<pw.Widget> combatGroups = [];
   roster.getCGs().forEach((cg) {
     combatGroups.add(buildCombatGroup(font, cg));
@@ -18,12 +19,34 @@ List<pw.Widget> buildCombatGroups(pw.Font font, UnitRoster roster) {
 }
 
 pw.Widget buildCombatGroup(pw.Font font, CombatGroup cg) {
-  final standardTextStyle =
-      pw.TextStyle(font: font, fontSize: _standardTextSize);
+//  final standardTextStyle =
+//      pw.TextStyle(font: font, fontSize: _standardTextSize);
   final smallTextStyle = pw.TextStyle(font: font, fontSize: _smallTextSize);
 
-  return pw.Text(
-    'Content for ${cg.name} to be loaded here',
-    style: standardTextStyle,
-  );
+  final primary = pw.Row(children: [
+    pw.Transform.rotateBox(
+        angle: pi / 2,
+        child: pw.Text(
+          '${cg.name} Primary',
+          style: smallTextStyle,
+        ))
+  ]);
+
+  final secondary = pw.Row(children: [
+    pw.Transform.rotateBox(
+        angle: pi / 2,
+        child: pw.Text(
+          '${cg.name} Seondary',
+          style: smallTextStyle,
+        ))
+  ]);
+
+  final result = pw.Column(children: [
+    primary,
+    primarySecondaryDivide,
+    secondary,
+    groupDivide,
+  ]);
+
+  return result;
 }
