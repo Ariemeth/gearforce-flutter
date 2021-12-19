e952847947b1eebf65726c4d55de15d128609ebc
Edward Carmack <ariemethacc@gmail.com>
2021-12-19 14:33:48 -0500

    added stubs for the combat groups pdf generation

--- lib/screens/roster/pdf/record_sheet/combat_groups.dart
+++ lib/screens/roster/pdf/record_sheet/combat_groups.dart
@@ -1,14 +1,29 @@
+import 'package:gearforce/models/combatGroups/combat_group.dart';
 import 'package:gearforce/models/roster/roster.dart';
 import 'package:pdf/widgets.dart' as pw;
 
+const double _standardTextSize = 12;
+const double _smallTextSize = 8;
+
 List<pw.Widget> buildCombatGroups(pw.Font font, UnitRoster roster) {
-  return [
-    pw.Text(
-      'Content',
-      style: pw.TextStyle(
-        font: font,
-        fontSize: 12,
-      ),
-    ),
-  ];
+  final standardTextStyle =
+      pw.TextStyle(font: font, fontSize: _standardTextSize);
+  final smallTextStyle = pw.TextStyle(font: font, fontSize: _smallTextSize);
+
+  final List<pw.Widget> combatGroups = [];
+  roster.getCGs().forEach((cg) {
+    combatGroups.add(buildCombatGroup(font, cg));
+  });
+  return combatGroups;
+}
+
+pw.Widget buildCombatGroup(pw.Font font, CombatGroup cg) {
+  final standardTextStyle =
+      pw.TextStyle(font: font, fontSize: _standardTextSize);
+  final smallTextStyle = pw.TextStyle(font: font, fontSize: _smallTextSize);
+
+  return pw.Text(
+    'Content for ${cg.name} to be loaded here',
+    style: standardTextStyle,
+  );
 }
--- lib/screens/roster/pdf/record_sheet/record_sheet.dart
+++ lib/screens/roster/pdf/record_sheet/record_sheet.dart
@@ -3,10 +3,12 @@ import 'package:gearforce/screens/roster/pdf/record_sheet/combat_groups.dart';
 import 'package:gearforce/screens/roster/pdf/record_sheet/header.dart';
 import 'package:pdf/widgets.dart' as pw;
 
+final sectionDivide = pw.Padding(padding: pw.EdgeInsets.only(bottom: 10.0));
+
 List<pw.Widget> buildRecordSheet(pw.Font font, UnitRoster roster) {
   return [
     buildRosterHeader(font, roster),
-    pw.Spacer(),
+    sectionDivide,
     ...buildCombatGroups(font, roster),
   ];
 }
