ffc91b6941ac2d604fd74923f93e17975976e042
Edward Carmack <ariemethacc@gmail.com>
2022-02-07 17:02:10 -0500

    Stubbed in functions to create all of the unit card sections

--- lib/screens/roster/pdf/unit_cards/unit_cards.dart
+++ lib/screens/roster/pdf/unit_cards/unit_cards.dart
@@ -8,6 +8,8 @@ const double _standardTextSize = 10;
 //const double _smallTextSize = 8;
 const double _cornerRadius = 5.0;
 const double _borderThickness = 1.0;
+const double _roleRowPadding = 5.0;
+const double _nameRowPadding = 5.0;
 
 final primarySecondaryDivide =
     pw.Padding(padding: pw.EdgeInsets.only(bottom: 5.0));
@@ -34,70 +36,17 @@ List<pw.Widget> _buildUnitCards(pw.Font font, List<Unit> units) {
 }
 
 pw.Widget _buildUnitCard(pw.Font font, Unit u) {
-  final titleTextStyle = pw.TextStyle(
-    font: font,
-    fontSize: _headerTextSize,
-    fontWeight: pw.FontWeight.bold,
-  );
-
-  final standardTextStyle = pw.TextStyle(
-    font: font,
-    fontSize: _standardTextSize,
-  );
-
   return pw.Container(
     width: 250.0,
     height: 300.0,
     child: pw.Column(
       crossAxisAlignment: pw.CrossAxisAlignment.stretch,
       children: [
-        // Name box
-        pw.Container(
-          child: pw.Padding(
-            padding: pw.EdgeInsets.all(5.0),
-            child: pw.Text(
-              u.name,
-              style: titleTextStyle,
-              textAlign: pw.TextAlign.center,
-            ),
-          ),
-          decoration: pw.BoxDecoration(
-            border: pw.Border(
-              bottom: pw.BorderSide(
-                width: _borderThickness,
-              ),
-            ),
-          ),
-        ),
-        // Role, TV, and Type boxes
-        pw.Container(
-          child: pw.Row(
-            mainAxisAlignment: pw.MainAxisAlignment.spaceEvenly,
-            children: [
-              pw.Container(
-                child: pw.Text(
-                  'Roles: ${u.role()!.roles.join(', ')}',
-                  style: standardTextStyle,
-                  textAlign: pw.TextAlign.center,
-                ),
-              ),
-              pw.Container(
-                child: pw.Text(
-                  'TV: ${u.tv}',
-                  style: standardTextStyle,
-                  textAlign: pw.TextAlign.center,
-                ),
-              ),
-              pw.Container(
-                child: pw.Text(
-                  'Type: ${u.type} ${u.core.height}',
-                  style: standardTextStyle,
-                  textAlign: pw.TextAlign.center,
-                ),
-              ),
-            ],
-          ),
-        ),
+        _buildFirstSectionBlock(font, u),
+        _buildSecondSectionBlock(font, u),
+        _buildThirdSectionBlock(font, u),
+        _buildTraitsBlock(font, u),
+        _buildWeaponsBlock(font, u),
       ],
     ),
     decoration: pw.BoxDecoration(
@@ -110,4 +59,111 @@ pw.Widget _buildUnitCard(pw.Font font, Unit u) {
       ),
     ),
   );
+}
+
+pw.Widget _buildFirstSectionBlock(pw.Font font, Unit u) {
+  final titleTextStyle = pw.TextStyle(
+    font: font,
+    fontSize: _headerTextSize,
+    fontWeight: pw.FontWeight.bold,
+  );
+
+  return pw.Container(
+    child: pw.Padding(
+      padding: pw.EdgeInsets.all(_nameRowPadding),
+      child: pw.Text(
+        u.name,
+        style: titleTextStyle,
+        textAlign: pw.TextAlign.center,
+      ),
+    ),
+    decoration: pw.BoxDecoration(
+      border: pw.Border(
+        bottom: pw.BorderSide(
+          width: _borderThickness,
+        ),
+      ),
+    ),
+  );
+}
+
+pw.Widget _buildSecondSectionBlock(pw.Font font, Unit u) {
+  final standardTextStyle = pw.TextStyle(
+    font: font,
+    fontSize: _standardTextSize,
+  );
+  return pw.Container(
+    child: pw.Padding(
+      padding:
+          pw.EdgeInsets.only(top: _roleRowPadding, bottom: _roleRowPadding),
+      child: pw.Row(
+        mainAxisAlignment: pw.MainAxisAlignment.spaceEvenly,
+        children: [
+          pw.Container(
+            child: pw.Text(
+              'Roles: ${u.role()!.roles.join(', ')}',
+              style: standardTextStyle,
+              textAlign: pw.TextAlign.center,
+            ),
+          ),
+          pw.Container(
+            child: pw.Text(
+              'TV: ${u.tv}',
+              style: standardTextStyle,
+              textAlign: pw.TextAlign.center,
+            ),
+          ),
+          pw.Container(
+            child: pw.Text(
+              'Type: ${u.type} ${u.core.height}',
+              style: standardTextStyle,
+              textAlign: pw.TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    ),
+    decoration: pw.BoxDecoration(
+      border: pw.Border(
+        bottom: pw.BorderSide(
+          width: _borderThickness,
+        ),
+      ),
+    ),
+  );
+}
+
+pw.Widget _buildThirdSectionBlock(pw.Font font, Unit u) {
+  return pw.Row(
+    children: [
+      _buildFirstStatBlock(font, u),
+      _buildSecondStatBlock(font, u),
+      _buildHullBlock(font, u),
+      _buildStructureBlock(font, u),
+    ],
+  );
+}
+
+pw.Widget _buildFirstStatBlock(pw.Font font, Unit u) {
+  return pw.Container();
+}
+
+pw.Widget _buildSecondStatBlock(pw.Font font, Unit u) {
+  return pw.Container();
+}
+
+pw.Widget _buildHullBlock(pw.Font font, Unit u) {
+  return pw.Container();
+}
+
+pw.Widget _buildStructureBlock(pw.Font font, Unit u) {
+  return pw.Container();
+}
+
+pw.Widget _buildTraitsBlock(pw.Font font, Unit u) {
+  return pw.Container();
+}
+
+pw.Widget _buildWeaponsBlock(pw.Font font, Unit u) {
+  return pw.Container();
 }
