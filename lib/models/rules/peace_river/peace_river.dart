3f4768084ebac23e5d28b45540c27eed45f5b7b5
Edward Carmack <ariemeth@gmail.com>
2022-11-22 22:40:53 -0500

    moved peace river rule logic into peace river rulest instead of base ruleset

--- lib/models/rules/peace_river/peace_river.dart
+++ lib/models/rules/peace_river/peace_river.dart
@@ -1,11 +1,51 @@
 import 'package:gearforce/data/unit_filter.dart';
-import 'package:gearforce/models/factions/faction_type.dart';
+import 'package:gearforce/models/combatGroups/combat_group.dart';
+import 'package:gearforce/models/combatGroups/group.dart';
 import 'package:gearforce/models/factions/faction_rule.dart';
+import 'package:gearforce/models/factions/faction_type.dart';
+import 'package:gearforce/models/mods/factionUpgrades/faction_mod.dart';
+import 'package:gearforce/models/mods/factionUpgrades/peace_river.dart';
+import 'package:gearforce/models/roster/roster.dart';
+import 'package:gearforce/models/rules/combat_group_options.dart';
+import 'package:gearforce/models/rules/peace_river/poc.dart' as poc;
+import 'package:gearforce/models/rules/peace_river/pps.dart' as pps;
+import 'package:gearforce/models/rules/peace_river/prdf.dart' as prdf;
 import 'package:gearforce/models/rules/rule_set.dart';
 import 'package:gearforce/models/rules/special_unit_filter.dart';
 import 'package:gearforce/models/unit/model_type.dart';
+import 'package:gearforce/models/unit/role.dart';
+import 'package:gearforce/models/unit/unit.dart';
 
 const String _baseRuleId = 'rule::peaceriver';
+
+final ruleArchitects = FactionRule(
+    name: 'Architects',
+    id: '$_baseRuleId::architects',
+    duelistCheck: (roster, u) {
+      return (u.type == ModelType.Gear || u.type == ModelType.Strider);
+    },
+    description: 'The duelist for this force may use a Peace River strider.');
+
+final ruleCrisisResponders = FactionRule(
+    name: 'Crisis Responders',
+    id: '$_baseRuleId::crisisResponders',
+    description:
+        'Any Crusader IV that has been upgraded to a Crusader V may swap their HAC, MSC, MBZ or LFG for a MPA (React) and a Shield for 1 TV. This Crisis Responder variant is unlimited for this force.');
+final ruleEPex = FactionRule(
+    name: 'E-pex',
+    id: '$_baseRuleId::ePex',
+    description:
+        'One Peace River model within each combat group may increase its EW skill by one for 1 TV each.');
+final ruleLaserTech = FactionRule(
+    name: 'Laser Tech',
+    id: '$_baseRuleId::laserTech',
+    description:
+        'Veteran universal infantry and veteran Spitz Monowheels may upgrade their IW, IR or IS for 1 TV each. These weapons receive the Advanced trait.');
+final ruleWarriorElite = FactionRule(
+    name: 'Warrior Elite',
+    id: '$_baseRuleId::warriorElite',
+    description:
+        'Any Warrior IV may be upgraded to a Warrior Elite for 1 TV each. This upgrade gives the Warrior IV a H/S of 4/2, an EW skill of 4+, and the Agile trait.');
 
 /*
   All the models in the Peace River Model List can be used in any of the sub-lists below. There are also models in the
@@ -30,22 +70,48 @@ class PeaceRiver extends RuleSet {
     ruleArchitects..addListener(() => notifyListeners());
   }
 
-  @override
-  List<SpecialUnitFilter> availableUnitFilters() {
-    return [
-      const SpecialUnitFilter(
-        text: coreName,
-        id: coreTag,
-        filters: const [
-          const UnitFilter(FactionType.PeaceRiver),
-          const UnitFilter(FactionType.Airstrike),
-          const UnitFilter(FactionType.Universal),
-          const UnitFilter(FactionType.Universal_TerraNova),
-          const UnitFilter(FactionType.Terrain),
-        ],
-      ),
-      ...super.availableUnitFilters(),
+  List<FactionModification> availableFactionMods(
+      UnitRoster ur, CombatGroup cg, Unit u) {
+    List<FactionModification> results = [
+      PeaceRiverFactionMods.e_pex(),
+      PeaceRiverFactionMods.warriorElite(),
+      PeaceRiverFactionMods.crisisResponders(u),
+      PeaceRiverFactionMods.laserTech(u),
     ];
+
+    // PRDF faction rules
+    var rule = FactionRule.findRule(factionUprades, prdf.ruleOlTrusty.id);
+    if (rule != null && rule.isEnabled) {
+      results.add(PeaceRiverFactionMods.olTrusty());
+    }
+    rule = FactionRule.findRule(factionUprades, prdf.ruleThunderFromTheSky.id);
+    if (rule != null && rule.isEnabled) {
+      results.add(PeaceRiverFactionMods.thunderFromTheSky());
+    }
+    rule = FactionRule.findRule(factionUprades, prdf.ruleEliteElements.id);
+    if (rule != null && rule.isEnabled) {
+      results.add(PeaceRiverFactionMods.eliteElements(ur));
+    }
+
+    // POC faction rules
+    rule = FactionRule.findRule(factionUprades, poc.ruleECMSpecialist.id);
+    if (rule != null && rule.isEnabled) {
+      results.add(PeaceRiverFactionMods.ecmSpecialist());
+    }
+    rule = FactionRule.findRule(factionUprades, poc.rulePOCOlTrusty.id);
+    if (rule != null && rule.isEnabled) {
+      results.add(PeaceRiverFactionMods.olTrustyPOC());
+    }
+    rule = FactionRule.findRule(factionUprades, poc.rulePeaceOfficer.id);
+    if (rule != null && rule.isEnabled) {
+      results.add(PeaceRiverFactionMods.peaceOfficers(u));
+    }
+    rule = FactionRule.findRule(factionUprades, poc.ruleGSwatSniper.id);
+    if (rule != null && rule.isEnabled) {
+      results.add(PeaceRiverFactionMods.gSWATSniper());
+    }
+
+    return [...results, ...super.availableFactionMods(ur, cg, u)];
   }
 
   @override
@@ -56,32 +122,175 @@ class PeaceRiver extends RuleSet {
         ruleLaserTech,
         ruleArchitects,
       ];
-}
 
-final ruleEPex = FactionRule(
-    name: 'E-pex',
-    id: '$_baseRuleId::ePex',
-    description:
-        'One Peace River model within each combat group may increase its EW skill by one for 1 TV each.');
-final ruleWarriorElite = FactionRule(
-    name: 'Warrior Elite',
-    id: '$_baseRuleId::warriorElite',
-    description:
-        'Any Warrior IV may be upgraded to a Warrior Elite for 1 TV each. This upgrade gives the Warrior IV a H/S of 4/2, an EW skill of 4+, and the Agile trait.');
-final ruleCrisisResponders = FactionRule(
-    name: 'Crisis Responders',
-    id: '$_baseRuleId::crisisResponders',
-    description:
-        'Any Crusader IV that has been upgraded to a Crusader V may swap their HAC, MSC, MBZ or LFG for a MPA (React) and a Shield for 1 TV. This Crisis Responder variant is unlimited for this force.');
-final ruleLaserTech = FactionRule(
-    name: 'Laser Tech',
-    id: '$_baseRuleId::laserTech',
-    description:
-        'Veteran universal infantry and veteran Spitz Monowheels may upgrade their IW, IR or IS for 1 TV each. These weapons receive the Advanced trait.');
-final ruleArchitects = FactionRule(
-    name: 'Architects',
-    id: '$_baseRuleId::architects',
-    duelistCheck: (roster, u) {
-      return (u.type == ModelType.Gear || u.type == ModelType.Strider);
-    },
-    description: 'The duelist for this force may use a Peace River strider.');
+  @override
+  List<SpecialUnitFilter> availableUnitFilters() {
+    final filters = [
+      const SpecialUnitFilter(
+        text: coreName,
+        id: coreTag,
+        filters: const [
+          const UnitFilter(FactionType.PeaceRiver),
+          const UnitFilter(FactionType.Airstrike),
+          const UnitFilter(FactionType.Universal),
+          const UnitFilter(FactionType.Universal_TerraNova),
+          const UnitFilter(FactionType.Terrain),
+        ],
+      )
+    ];
+
+    var rule =
+        FactionRule.findRule(factionUprades, prdf.ruleBestMenAndWomen.id);
+    if (rule != null && rule.isEnabled) {
+      filters.add(prdf.filterBestMenAndWomen);
+    }
+
+    rule = FactionRule.findRule(factionUprades, poc.ruleMercenaryContract.id);
+    if (rule != null && rule.isEnabled) {
+      filters.add(poc.filterMercContract);
+    }
+
+    rule = FactionRule.findRule(factionUprades, pps.ruleSubContractors.id);
+    if (rule != null && rule.isEnabled) {
+      filters.add(pps.filterSubContractor);
+    }
+
+    return [...filters, ...super.availableUnitFilters()];
+  }
+
+  @override
+  bool canBeAddedToGroup(Unit unit, Group group, CombatGroup cg) {
+    var rule = FactionRule.findRule(factionUprades, pps.ruleSubContractors.id);
+    if (rule != null &&
+        rule.isEnabled &&
+        rule.canBeAddedToGroup != null &&
+        !rule.canBeAddedToGroup!(unit, group, cg)) {
+      return false;
+    }
+    return super.canBeAddedToGroup(unit, group, cg);
+  }
+
+  @override
+  CombatGroupOption combatGroupSettings() {
+    final options = CombatGroupOption(name: 'Rule Options', options: []);
+
+    var rule = FactionRule.findRule(factionUprades, pps.ruleSubContractors.id);
+    if (rule != null && rule.isEnabled) {
+      options.options.add(Option(
+          name: pps.ruleSubContractors.name,
+          id: pps.ruleSubContractors.id,
+          requirementCheck: onlyOneCG(
+            pps.ruleSubContractors.id,
+          )));
+    }
+
+    rule = FactionRule.findRule(factionUprades, pps.ruleBadlandsSoup.id);
+    if (rule != null && rule.isEnabled) {
+      options.options.add(Option(
+          name: pps.ruleBadlandsSoup.name,
+          id: pps.ruleBadlandsSoup.id,
+          requirementCheck: onlyOneCG(
+            pps.ruleBadlandsSoup.id,
+          )));
+    }
+
+    rule = FactionRule.findRule(factionUprades, poc.ruleMercenaryContract.id);
+    if (rule != null && rule.isEnabled) {
+      options.options.add(Option(
+        name: poc.ruleMercenaryContract.name,
+        id: poc.ruleMercenaryContract.id,
+      ));
+    }
+
+    return options;
+  }
+
+  @override
+  bool duelistCheck(UnitRoster roster, Unit u) {
+    final rule = FactionRule.findRule(factionUprades, ruleArchitects.id);
+    if (rule != null && rule.isEnabled) {
+      if (!rule.duelistCheck!(roster, u)) {
+        return false;
+      }
+    } else if (u.type != ModelType.Gear) {
+      return false;
+    }
+
+    // only 1 duelist is allowed.
+    return !roster.hasDuelist();
+  }
+
+  @override
+  bool hasGroupRole(Unit unit, RoleType target) {
+    var rule = FactionRule.findRule(factionUprades, poc.ruleSpecialIssue.id);
+    if (rule != null &&
+        rule.isEnabled &&
+        rule.hasGroupRole != null &&
+        rule.hasGroupRole!(unit, target)) {
+      return true;
+    }
+
+    return super.hasGroupRole(unit, target);
+  }
+
+  @override
+  bool isRoleTypeUnlimited(Unit unit, RoleType target, Group group) {
+    var rule = FactionRule.findRule(factionUprades, prdf.ruleHighTech.id);
+    if (rule != null &&
+        rule.isEnabled &&
+        rule.isRoleTypeUnlimited != null &&
+        rule.isRoleTypeUnlimited!(unit, target, group)) {
+      return true;
+    }
+
+    return super.isRoleTypeUnlimited(unit, target, group);
+  }
+
+  @override
+  bool isUnitCountWithinLimits(CombatGroup cg, Group group, Unit unit) {
+    if (unit.hasTag(prdf.ruleBestMenAndWomen.id)) {
+      final rule = FactionRule.findRule(
+        factionUprades,
+        prdf.ruleBestMenAndWomen.id,
+      )?.isUnitCountWithinLimits;
+      if (rule != null) {
+        return rule(cg, group, unit);
+      }
+    }
+
+    return super.isUnitCountWithinLimits(cg, group, unit);
+  }
+
+  @override
+  int modCostOverride(int baseCost, String modID, Unit u) {
+    var rule = FactionRule.findRule(factionUprades, poc.ruleGSwatSniper.id);
+    if (rule != null && rule.isEnabled && rule.modCostOverride != null) {
+      return rule.modCostOverride!(baseCost, modID, u);
+    }
+
+    return super.modCostOverride(baseCost, modID, u);
+  }
+
+  @override
+  bool veteranModCheck(Unit u, CombatGroup cg, {required String modID}) {
+    if (cg.hasTag(pps.ruleBadlandsSoup.id)) {
+      var rule = FactionRule.findRule(factionUprades, pps.ruleBadlandsSoup.id);
+      if (rule != null &&
+          rule.isEnabled &&
+          rule.veteranModCheck != null &&
+          rule.veteranModCheck!(u, cg, modID: modID)) {
+        return true;
+      }
+    }
+
+    var rule = FactionRule.findRule(factionUprades, poc.ruleGSwatSniper.id);
+    if (rule != null &&
+        rule.isEnabled &&
+        rule.veteranModCheck != null &&
+        rule.veteranModCheck!(u, cg, modID: modID)) {
+      return true;
+    }
+
+    return super.veteranModCheck(u, cg, modID: modID);
+  }
+}
--- lib/models/rules/rule_set.dart
+++ lib/models/rules/rule_set.dart
@@ -5,13 +5,8 @@ import 'package:gearforce/models/combatGroups/group.dart';
 import 'package:gearforce/models/factions/faction_rule.dart';
 import 'package:gearforce/models/factions/faction_type.dart';
 import 'package:gearforce/models/mods/factionUpgrades/faction_mod.dart';
-import 'package:gearforce/models/mods/factionUpgrades/peace_river.dart';
 import 'package:gearforce/models/roster/roster.dart';
 import 'package:gearforce/models/rules/combat_group_options.dart';
-import 'package:gearforce/models/rules/peace_river/peace_river.dart' as pr;
-import 'package:gearforce/models/rules/peace_river/poc.dart' as poc;
-import 'package:gearforce/models/rules/peace_river/pps.dart' as pps;
-import 'package:gearforce/models/rules/peace_river/prdf.dart' as prdf;
 import 'package:gearforce/models/rules/special_unit_filter.dart';
 import 'package:gearforce/models/unit/model_type.dart';
 import 'package:gearforce/models/unit/role.dart';
@@ -62,67 +57,12 @@ abstract class RuleSet extends ChangeNotifier {
   int get maxPrimaryActions => _maxPrimaryActions;
   int get minPrimaryActions => _minPrimaryActions;
   List<FactionModification> availableFactionMods(
-      UnitRoster ur, CombatGroup cg, Unit u) {
-    List<FactionModification> results = [];
-
-    // Peace River faction rules
-    var rule = FactionRule.findRule(factionUprades, pr.ruleEPex.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(PeaceRiverFactionMods.e_pex());
-    }
-    rule = FactionRule.findRule(factionUprades, pr.ruleWarriorElite.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(PeaceRiverFactionMods.warriorElite());
-    }
-    rule = FactionRule.findRule(factionUprades, pr.ruleCrisisResponders.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(PeaceRiverFactionMods.crisisResponders(u));
-    }
-    rule = FactionRule.findRule(factionUprades, pr.ruleLaserTech.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(PeaceRiverFactionMods.laserTech(u));
-    }
-
-    // PRDF faction rules
-    rule = FactionRule.findRule(factionUprades, prdf.ruleOlTrusty.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(PeaceRiverFactionMods.olTrusty());
-    }
-    rule = FactionRule.findRule(factionUprades, prdf.ruleThunderFromTheSky.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(PeaceRiverFactionMods.thunderFromTheSky());
-    }
-    rule = FactionRule.findRule(factionUprades, prdf.ruleEliteElements.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(PeaceRiverFactionMods.eliteElements(ur));
-    }
-
-    return results;
-  }
+          UnitRoster ur, CombatGroup cg, Unit u) =>
+      [];
 
   List<FactionRule> availableFactionRules();
   List<FactionRule> availableSubFactionRules() => [];
-  List<SpecialUnitFilter> availableUnitFilters() {
-    final List<SpecialUnitFilter> results = [];
-
-    var rule =
-        FactionRule.findRule(factionUprades, prdf.ruleBestMenAndWomen.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(prdf.filterBestMenAndWomen);
-    }
-
-    rule = FactionRule.findRule(factionUprades, poc.ruleMercenaryContract.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(poc.filterMercContract);
-    }
-
-    rule = FactionRule.findRule(factionUprades, pps.ruleSubContractors.id);
-    if (rule != null && rule.isEnabled) {
-      results.add(pps.filterSubContractor);
-    }
-
-    return results;
-  }
+  List<SpecialUnitFilter> availableUnitFilters() => [];
 
   List<Unit> availableUnits({
     List<RoleType>? role,
@@ -159,14 +99,6 @@ abstract class RuleSet extends ChangeNotifier {
   }
 
   bool canBeAddedToGroup(Unit unit, Group group, CombatGroup cg) {
-    var rule = FactionRule.findRule(factionUprades, pps.ruleSubContractors.id);
-    if (rule != null &&
-        rule.isEnabled &&
-        rule.canBeAddedToGroup != null &&
-        !rule.canBeAddedToGroup!(unit, group, cg)) {
-      return false;
-    }
-
     final r = unit.role;
     final targetRole = group.role();
 
@@ -203,53 +135,11 @@ abstract class RuleSet extends ChangeNotifier {
         !unit.traits.any((t) => t.name == 'Conscript');
   }
 
-  CombatGroupOption combatGroupSettings() {
-    final options = CombatGroupOption(name: 'Rule Options', options: []);
-
-    var rule = FactionRule.findRule(factionUprades, pps.ruleSubContractors.id);
-    if (rule != null && rule.isEnabled) {
-      options.options.add(Option(
-          name: pps.ruleSubContractors.name,
-          id: pps.ruleSubContractors.id,
-          requirementCheck: onlyOneCG(
-            pps.ruleSubContractors.id,
-          )));
-    }
-
-    rule = FactionRule.findRule(factionUprades, pps.ruleBadlandsSoup.id);
-    if (rule != null && rule.isEnabled) {
-      options.options.add(Option(
-          name: pps.ruleBadlandsSoup.name,
-          id: pps.ruleBadlandsSoup.id,
-          requirementCheck: onlyOneCG(
-            pps.ruleBadlandsSoup.id,
-          )));
-    }
-
-    rule = FactionRule.findRule(factionUprades, poc.ruleMercenaryContract.id);
-    if (rule != null && rule.isEnabled) {
-      options.options.add(Option(
-        name: poc.ruleMercenaryContract.name,
-        id: poc.ruleMercenaryContract.id,
-      ));
-    }
-
-    return options;
-  }
+  CombatGroupOption combatGroupSettings() =>
+      CombatGroupOption(name: 'Rule Options', options: []);
 
   bool duelistCheck(UnitRoster roster, Unit u) {
-    /*
-      Architects: The duelist for this force may use a Peace River strider.
-    */
-    final rule = FactionRule.findRule(
-      factionUprades,
-      pr.ruleArchitects.id,
-    );
-    if (rule != null && rule.isEnabled) {
-      if (!rule.duelistCheck!(roster, u)) {
-        return false;
-      }
-    } else if (u.type != ModelType.Gear) {
+    if (u.type != ModelType.Gear) {
       return false;
     }
 
@@ -259,14 +149,6 @@ abstract class RuleSet extends ChangeNotifier {
 
   // Ensure the target Roletype is within the Roles
   bool hasGroupRole(Unit unit, RoleType target) {
-    var rule = FactionRule.findRule(factionUprades, poc.ruleSpecialIssue.id);
-    if (rule != null &&
-        rule.isEnabled &&
-        rule.hasGroupRole != null &&
-        rule.hasGroupRole!(unit, target)) {
-      return true;
-    }
-
     return unit.role == null ? false : unit.role!.includesRole([target]);
   }
 
@@ -281,14 +163,6 @@ abstract class RuleSet extends ChangeNotifier {
       return true;
     }
 
-    var rule = FactionRule.findRule(factionUprades, prdf.ruleHighTech.id);
-    if (rule != null &&
-        rule.isEnabled &&
-        rule.isRoleTypeUnlimited != null &&
-        rule.isRoleTypeUnlimited!(unit, target, group)) {
-      return true;
-    }
-
     return false;
   }
 
@@ -296,20 +170,6 @@ abstract class RuleSet extends ChangeNotifier {
       FactionRule.isRuleEnabled(factionUprades, ruleName);
 
   bool isUnitCountWithinLimits(CombatGroup cg, Group group, Unit unit) {
-    /*
-       The Best Men and Women for the Job: One model in each combat group may
-       be selected from the Black Talon model list.
-    */
-    if (unit.hasTag(prdf.ruleBestMenAndWomen.id)) {
-      final rule = FactionRule.findRule(
-        factionUprades,
-        prdf.ruleBestMenAndWomen.id,
-      )?.isUnitCountWithinLimits;
-      if (rule != null) {
-        return rule(cg, group, unit);
-      }
-    }
-
     // get the number other instances of this unitcore in the group
     final count =
         group.allUnits().where((u) => u.core.name == unit.core.name).length;
@@ -320,34 +180,9 @@ abstract class RuleSet extends ChangeNotifier {
 
   int maxSecondaryActions(int primaryActions) => (primaryActions / 2).ceil();
 
-  int modCostOverride(int baseCost, String modID, Unit u) {
-    var rule = FactionRule.findRule(factionUprades, poc.ruleGSwatSniper.id);
-    if (rule != null && rule.isEnabled && rule.modCostOverride != null) {
-      return rule.modCostOverride!(baseCost, modID, u);
-    }
-
-    return baseCost;
-  }
+  int modCostOverride(int baseCost, String modID, Unit u) => baseCost;
 
   bool veteranModCheck(Unit u, CombatGroup cg, {required String modID}) {
-    if (cg.hasTag(pps.ruleBadlandsSoup.id)) {
-      var rule = FactionRule.findRule(factionUprades, pps.ruleBadlandsSoup.id);
-      if (rule != null &&
-          rule.isEnabled &&
-          rule.veteranModCheck != null &&
-          rule.veteranModCheck!(u, cg, modID: modID)) {
-        return true;
-      }
-    }
-
-    var rule = FactionRule.findRule(factionUprades, poc.ruleGSwatSniper.id);
-    if (rule != null &&
-        rule.isEnabled &&
-        rule.veteranModCheck != null &&
-        rule.veteranModCheck!(u, cg, modID: modID)) {
-      return true;
-    }
-
     return (u.traits.any((trait) => trait.name == 'Vet'));
   }
 
