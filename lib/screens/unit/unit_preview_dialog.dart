4c99a26c598fdcb9ff3531b3be73bc4eaa623f40
Edward Carmack <ariemeth@gmail.com>
2023-12-03 01:52:31 -0500

    Created a filled out unit card for the unit preview, except handling combo weapons

--- lib/screens/unit/unit_preview_dialog.dart
+++ lib/screens/unit/unit_preview_dialog.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gearforce/models/unit/command.dart';
 import 'package:gearforce/models/unit/unit.dart';
+import 'package:gearforce/models/weapons/weapon_modes.dart';
+
+const _horizontalBorder = const BorderSide();
+const _primaryStatWidth = 70.0;
+const _reactSymbol = '»';
 
 class UnitPreviewDialog extends StatelessWidget {
   final Unit unit;
@@ -45,35 +51,244 @@ class UnitPreviewDialog extends StatelessWidget {
     return dialog;
   }
 
-// TODO finish implementing
   Widget _nameRow(BuildContext context) {
-    return Text(unit.name);
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(unit.name),
+        ],
+      ),
+      decoration: BoxDecoration(
+        border: Border(bottom: _horizontalBorder),
+      ),
+    );
   }
 
   Widget _StatsRow(BuildContext context) {
-    return Row(
-      children: [
-        _PrimaryStats(context),
-        _SecondaryStats(context),
-      ],
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            width: _primaryStatWidth,
+            child: _PrimaryStats(context),
+            decoration: BoxDecoration(
+              border: Border(
+                right: BorderSide(),
+              ),
+            ),
+          ),
+          Flexible(
+            child: _SecondaryStats(context),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+          border: Border(
+        bottom: _horizontalBorder,
+      )),
     );
   }
 
   Widget _PrimaryStats(BuildContext context) {
-    return Column();
+    final gunnery = unit.gunnery == null ? '-' : '${unit.gunnery}+';
+    final piloting = unit.piloting == null ? '-' : '${unit.piloting}+';
+    final ew = unit.ew == null ? '-' : '${unit.ew}+';
+    final actions = unit.actions == null ? '-' : '${unit.actions}';
+    final armor = unit.armor == null ? '-' : '${unit.armor}';
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+      child: Column(
+        children: [
+          Table(
+            children: [
+              TableRow(children: [
+                Text('Gu:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text(gunnery),
+                ),
+              ]),
+              TableRow(children: [
+                Text('Pi:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text(piloting),
+                ),
+              ]),
+              TableRow(children: [
+                Text('Ew:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text(ew),
+                ),
+              ]),
+              TableRow(children: [
+                Text('A:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text(actions),
+                ),
+              ]),
+              TableRow(children: [
+                Text('Arm:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text(armor),
+                ),
+              ]),
+            ],
+            columnWidths: const <int, TableColumnWidth>{
+              0: FixedColumnWidth(35),
+            },
+          )
+        ],
+      ),
+    );
   }
 
   Widget _SecondaryStats(BuildContext context) {
-    return Column();
+    return Padding(
+      padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Table(
+            children: [
+              TableRow(children: [
+                Text('Gear:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text('${unit.height}"'),
+                ),
+              ]),
+              TableRow(children: [
+                Text('Cmd:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text('-'),
+                ),
+              ]),
+              TableRow(children: [
+                Text('MR:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text('${unit.movement}'),
+                ),
+              ]),
+              TableRow(children: [
+                Text('H:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text('${unit.hull == null ? '-' : '${unit.hull}'}'),
+                ),
+              ]),
+              TableRow(children: [
+                Text('S:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text(
+                      '${unit.structure == null ? '-' : '${unit.structure}'}'),
+                ),
+              ]),
+            ],
+            columnWidths: const <int, TableColumnWidth>{
+              0: FixedColumnWidth(35),
+              1: IntrinsicColumnWidth(),
+            },
+          ),
+          Spacer(),
+          Table(
+            children: [
+              TableRow(children: [
+                Text('TV:', textAlign: TextAlign.right),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text('${unit.tv}'),
+                ),
+              ]),
+              TableRow(children: [
+                Text(
+                  '${unit.commandLevel == CommandLevel.none ? 'SP' : 'CP'}:',
+                  textAlign: TextAlign.right,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text(
+                      '${unit.commandLevel == CommandLevel.none ? unit.skillPoints : unit.commandPoints}'),
+                ),
+              ]),
+            ],
+            columnWidths: const <int, TableColumnWidth>{
+              0: FixedColumnWidth(35),
+              1: IntrinsicColumnWidth(),
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _TraitsRow(BuildContext context) {
-    return Row(
-      children: [Text(unit.traits.join(', '))],
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+        child: Row(
+          children: [Text(unit.traits.join(', '))],
+        ),
+      ),
+      decoration: BoxDecoration(
+          border: Border(
+        bottom: _horizontalBorder,
+      )),
     );
   }
 
   Widget _WeaponsRow(BuildContext context) {
-    return Column();
+    final weaponTable = Table(
+      children: [
+        const TableRow(
+          children: [
+            Text('Code'),
+            Text('Range'),
+            Text('D'),
+            Text('Traits'),
+            Text('Mode')
+          ],
+        ),
+      ],
+      columnWidths: const <int, TableColumnWidth>{
+        0: FixedColumnWidth(50),
+        1: FixedColumnWidth(60),
+        2: FixedColumnWidth(15),
+        3: FlexColumnWidth(),
+        4: IntrinsicColumnWidth(),
+      },
+    );
+
+    // TODO Handle combo weapons
+    unit.weapons.forEach((w) {
+      weaponTable.children.add(TableRow(children: [
+        Text('${w.hasReact ? '$_reactSymbol' : ''}${w.abbreviation}'),
+        Text('${w.range}'),
+        Text('${w.damage}'),
+        Text('${w.traits.join(', ')}'),
+        Text(
+          '${w.modes.map((m) => getWeaponModeName(m)[0]).toList().join(', ')}',
+          textAlign: TextAlign.center,
+        )
+      ]));
+    });
+
+    final layout = Padding(
+        padding: const EdgeInsets.only(left: 5.0, right: 5.0, bottom: 5.0),
+        child: weaponTable);
+
+    return layout;
   }
 }
