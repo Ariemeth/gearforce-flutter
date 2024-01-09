a3eab00003e3535976ca009be97876dd5537eb97
Edward Carmack <ariemeth@gmail.com>
2024-01-09 17:34:26 -0500

    Added faction and subfaction rules to the printed/pdf rulesheet

--- lib/screens/roster/pdf/pdf.dart
+++ lib/screens/roster/pdf/pdf.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:file_selector/file_selector.dart';
 import 'package:gearforce/models/roster/roster.dart';
 import 'package:gearforce/screens/roster/pdf/record_sheet/record_sheet.dart';
+import 'package:gearforce/screens/roster/pdf/record_sheet/rules_sheet.dart';
 import 'package:gearforce/screens/roster/pdf/record_sheet/traits_sheet.dart';
 import 'package:gearforce/screens/roster/pdf/unit_cards/unit_cards.dart';
 import 'package:pdf/pdf.dart';
@@ -79,6 +80,16 @@ Future<Uint8List> buildPdf(PdfPageFormat format, UnitRoster roster,
       pageTheme: pageTheme,
       build: (context) {
         return [buildTraitSheet(font, roster.getAllUnits())];
+      },
+      footer: (pw.Context context) {
+        return _buildFooter(context, version, roster.rulesVersion);
+      }));
+
+  // Add Rules reference page
+  doc.addPage(pw.MultiPage(
+      pageTheme: pageTheme,
+      build: (context) {
+        return [buildRulesSheet(font, roster)];
       },
       footer: (pw.Context context) {
         return _buildFooter(context, version, roster.rulesVersion);
---
+++ lib/screens/roster/pdf/record_sheet/rules_sheet.dart
@@ -0,0 +1,83 @@
+import 'package:gearforce/models/roster/roster.dart';
+import 'package:gearforce/models/rules/faction_rule.dart';
+import 'package:pdf/widgets.dart' as pw;
+
+const double _headerTextSize = 12;
+const double _standardTextSize = 10;
+
+pw.Widget buildRulesSheet(pw.Font font, UnitRoster roster) {
+  // Faction rules
+  final List<(String, String)> factionRules = [];
+  FactionRule.enabledRules(roster.rulesetNotifer.value.factionRules)
+      .forEach((fr) {
+    factionRules.add((fr.name, fr.description));
+  });
+
+  final List<(String, String)> subFactionRules = [];
+  FactionRule.enabledRules(roster.rulesetNotifer.value.subFactionRules)
+      .forEach((fr) {
+    subFactionRules.add((fr.name, fr.description));
+  });
+
+  // CG special rules
+
+  // Unit special rules
+
+  final sheet = pw.Column(children: [
+    _buildRuleTable(font, ['Faction Rule', 'Description'], factionRules),
+    _buildRuleTable(font, ['Sub-Faction Rule', 'Description'], subFactionRules),
+  ]);
+
+  return pw.Padding(
+    padding: pw.EdgeInsets.only(
+      left: 10.0,
+      right: 10.0,
+      top: 10.0,
+      bottom: 10.0,
+    ),
+    child: sheet,
+  );
+}
+
+pw.Widget _buildRuleTable(
+  pw.Font font,
+  List<String> tableHeaders,
+  List<(String, String)> rules,
+) {
+  if (rules.isEmpty) {
+    return pw.Container();
+  }
+  var table = pw.TableHelper.fromTextArray(
+    cellStyle: pw.TextStyle(
+      font: font,
+      fontSize: _standardTextSize,
+    ),
+    columnWidths: {
+      0: const pw.FixedColumnWidth(200.0),
+    },
+    headers: tableHeaders,
+    headerStyle: pw.TextStyle(
+      font: font,
+      fontSize: _headerTextSize,
+      fontWeight: pw.FontWeight.bold,
+    ),
+    data: List<List<String>>.generate(
+      rules.length,
+      (row) => List<String>.generate(tableHeaders.length, (col) {
+        switch (col) {
+          case 0:
+            return rules[row].$1;
+          case 1:
+            return rules[row].$2;
+          default:
+            return '';
+        }
+      }),
+    ),
+  );
+  return pw.Padding(
+      padding: pw.EdgeInsets.only(
+        bottom: 10.0,
+      ),
+      child: table);
+}
