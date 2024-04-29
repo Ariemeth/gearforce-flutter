bb161d9729e9859e9608d2f5c498580d77ce2386
Edward Carmack <ariemethacc@gmail.com>
2024-04-29 03:07:01 -0400

    changing the way the version uri is generated

--- lib/widgets/api/api_service.dart
+++ lib/widgets/api/api_service.dart
@@ -53,7 +53,7 @@ class ApiService {
   static Future<String?> getLatestVersion(Uri baseUri) async {
     print('base: ${baseUri.host}');
     final uri = switch (baseUri.scheme) {
-      'https' => Uri.parse('${baseUri.host}/version.json'),
+      'https' => baseUri.replace(path: '/version.json'),
       _ => Uri.parse('$_default_base_url/version.json'),
     };
 
